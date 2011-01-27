c3820485441e460c8a4f9ef27887228812df7462
Kyle Warren <qarren@kyle-warrens-macbook-pro.local>
2011-01-26 21:54:58 -0800

    Fixed some stuff

--- appengine_stuff/static/js/sc.js
+++ appengine_stuff/static/js/sc.js
@@ -1,14 +1,27 @@
-$("#connect2").click(function () {
-      var id = generateID();
-      $("id").val(id);
-      alert('test');
+(function (window, $) {
 
-	function generateID () {
-	    var text = "";
-	    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	    for( var i=0; i < 5; i++ )
-	        text += possible.charAt(Math.floor(Math.random() * possible.length));
-	    return text;
-	    console.log('test2');
-	}
+var SC = window.SC = {};
+
+SC.log = function () {
+    if (console) {
+        console.log(arguments);
+    }
+};
+
+function generateID() {
+    var text = "",
+        possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        
+    for (var i=0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    
+    return text;
+}
+
+$("#connect_form").submit(function (event) {
+    var id = generateID();
+    $("#uid").val(id);
 });
+
+}(window, jQuery));
